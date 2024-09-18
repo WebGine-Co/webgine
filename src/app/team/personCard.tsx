f73c990ac76120bd656b2a458871cff16d50a239
Thibault Vercoutre <vercoutre.thibault@gmail.com>
2024-09-18 19:57:48 -0400

    skills

--- src/app/team/personCard.tsx
+++ src/app/team/personCard.tsx
@@ -4,20 +4,31 @@ interface PersonCardProps {
     firstname: string;
     lastname: string;
     links: string[];
+    skills: {name: string, color: string}[];
 }
 
-export default function PersonCard({ firstname, lastname, links }: PersonCardProps) {
+export default function PersonCard({ firstname, lastname, links, skills }: PersonCardProps) {
     return (
         <div className="card bg-base-100 md:w-96 w-72 shadow-xl shadow-primary mx-auto">
-            <figure className="my-2">
-                <div className="avatar placeholder">
-                    <div className="bg-neutral text-neutral-content w-24 rounded-full">
-                        <span className="text-3xl">{ firstname[0] }</span>
+            <figure className="py-4">
+                <div className="indicator">
+                    <span className="indicator-item badge badge-accent indicator-bottom indicator-end">Co-fondateur</span>
+                    <div className="place-items-center">
+                        <div className="avatar placeholder">
+                            <div className="bg-neutral text-neutral-content w-24 rounded-full">
+                                <span className="text-3xl">{ firstname[0] }</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{ firstname } { lastname }</h2>
+                <p>
+                    { skills.map((skill, index) => (
+                        <span key={index} className={`badge badge-outline badge-${skill.color}`}>{ skill.name }</span>
+                    )) }
+                </p>
                 {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
                 <div className="card-actions justify-end">
                     <ModalContact classNameModal={`${firstname}-${lastname}`} links={links} />
--- src/app/team/team.tsx
+++ src/app/team/team.tsx
@@ -3,8 +3,32 @@ import PersonCard from "./personCard";
 export default function Team() {
     return (
       <div className="py-20 grid gap-4 md:grid-cols-2 grid-cols-1">
-        <PersonCard firstname="Thibault" lastname="Vercoutre" links={["https://github.com/ThibaultVercoutre", "https://www.linkedin.com/in/thibault-vercoutre-5072b1226/"]} />
-        <PersonCard firstname="Julien" lastname="Konieczny" links={["https://github.com/julienk62", "https://www.linkedin.com/in/julienkonieczny/"]}/>
+        <PersonCard 
+          firstname="Thibault" 
+          lastname="Vercoutre" 
+          links={["https://github.com/ThibaultVercoutre", "https://www.linkedin.com/in/thibault-vercoutre-5072b1226/"]}
+          skills={[
+            {name: "Full Stack Dev", color: "accent"},
+            {name: "React", color: "secondary"},
+            {name: "Node.js", color: "secondary"},
+            {name: "Angular", color: "secondary"},
+            {name: "NextJS", color: "secondary"},
+            {name: "Symfony", color: "secondary"},
+            {name: "Python", color: "primary"},
+          ]} />
+
+        <PersonCard 
+          firstname="Julien" 
+          lastname="Konieczny" 
+          links={["https://github.com/julienk62", "https://www.linkedin.com/in/julienkonieczny/"]}
+          skills={[
+            {name: "Full Stack Dev", color: "accent"},
+            {name: "React", color: "secondary"},
+            {name: "Node.js", color: "secondary"},
+            {name: "Angular", color: "secondary"},
+            {name: "Symfony", color: "secondary"},
+            {name: "Python", color: "primary"},
+          ]}/>
       </div>
     );
   }
