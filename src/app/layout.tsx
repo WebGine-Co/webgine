84651b7d8a13589cc3111079eb0c07dfc230d24e
Thibault Vercoutre <vercoutre.thibault@gmail.com>
2024-12-04 19:01:52 -0500

    fix dvh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="night">
-      <body className='min-h-screen 2xl:mx-96 xl:mx-72 lg:mx-60 md:mx-32 sm:mx-10 mx-5 flex flex-col'>
+      <body className='min-h-dvh 2xl:mx-96 xl:mx-72 lg:mx-60 md:mx-32 sm:mx-10 mx-5 flex flex-col'>
         <Navbar />
         <main className="flex-1 flex">
           {children}
